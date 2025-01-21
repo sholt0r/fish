ee02cad4cef36f602db0e516cfa486c6c03fdab5
sholt0r <sholtor@kobu.au>
2025-01-21 14:06:51 +1100

    Added go dir to path

--- config.fish
+++ config.fish
@@ -5,3 +5,6 @@ end
 starship init fish | source
 zoxide init fish | source
 fish_ssh_agent
+
+# Generated for envman. Do not edit.
+test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
