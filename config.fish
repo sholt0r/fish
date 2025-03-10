fa3dbdfaae7c6b91304b2e7ad0cb8d8c7bf0f122
sholt0r <sholtor@kobu.au>
2025-03-10 20:15:44 +1100

    Changes to fish config

--- config.fish
+++ config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -x SSH_AUTH_SOCK /Users/jstaples/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
+
 starship init fish | source
 zoxide init fish | source
 fish_ssh_agent
