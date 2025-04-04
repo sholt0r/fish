ad80e643291b415f55838d7970c0b8487e6b0987
sholt0r <sholtor@kobu.au>
2025-04-04 23:49:52 +1100

    Update fish

--- config.fish
+++ config.fish
@@ -7,3 +7,17 @@ set -x SSH_AUTH_SOCK /Users/jstaples/Library/Containers/com.maxgoedjen.Secretive
 starship init fish | source
 zoxide init fish | source
 fish_ssh_agent
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
